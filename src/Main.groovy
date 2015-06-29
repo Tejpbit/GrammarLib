e719d22643f499fe2491f54a07bd5016c36630d3
André Samuelsson <andresamuelsson94@gmail.com>
2015-06-29 21:49:08 +0200

    implemented CYK table filling algorithm. Seems to work

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,11 +1,13 @@
 import groovy.transform.ToString
 
+import java.util.stream.Collectors
+
 /**
  * Created by tejp on 29/06/15.
  */
 
 class Grammar {
-    List<Rule> rules
+    List<Rule> rules = new ArrayList<>()
 
     void addRule(Rule rule) {
         rules << rule
@@ -15,11 +17,61 @@ class Grammar {
 
     }
 
-    
+    CYKResult getCYKResult(String str) {
+        new CYKResult(str, this)
+    }
+
+    Set<Rule> findRulesFor(String ruleResult) {
+        rules.stream()
+            .filter({it.values.contains(ruleResult)})
+            .collect(Collectors.toSet())
+    }
 }
 
 class CYKResult {
 
+    CYKElement[][] table
+
+    CYKResult(String str, Grammar grammar) {
+        table = new CYKElement[str.length()][]
+
+        for (int row = 0; row < str.length(); row++) {
+            table[row] = new CYKElement[str.length() - row]
+            for (int col = 0; col < table[row].length ; col++) {
+                table[row][col] = new CYKElement(subWord: str.substring(col, col+row+1 ))
+
+                table[row][col].findWays(row, col, grammar, table)
+            }
+        }
+
+    }
+
+    @ToString
+    static class CYKElement {
+        Set<Rule> rules = new HashSet<>()
+        String subWord
+
+        void findWays(int row, int col, Grammar grammar, CYKElement[][] table) {
+
+            if (row == 0) {
+                rules.addAll(grammar.findRulesFor(subWord))
+            }
+
+            for (int i = 1; i <= subWord.length() - 1 ; i++) {
+
+                CYKElement left  = table[i-1][col]
+                CYKElement right = table[row-i][col+i]
+
+                left.rules.each { leftIT ->
+                    right.rules.each { rightIT ->
+                        String combined = leftIT.name + rightIT.name
+                        rules.addAll(grammar.findRulesFor(combined))
+                    }
+                }
+            }
+
+        }
+    }
 }
 
 @ToString
@@ -37,7 +89,7 @@ class GrammarDelegate {
 @ToString
 class Rule {
     String name
-    List<String> values = new ArrayList<>()
+    Set<String> values = new HashSet<>()
 }
 
 class RuleBuilder {
@@ -57,11 +109,23 @@ class RuleBuilder {
 GrammarDelegate g = new GrammarDelegate();
 
 g.with {
-    var 'S' becomes "AB"  or "C"
-    var 'A' becomes "aAb" or "ab"
-    var 'B' becomes "cBd" or "cd"
-    var 'C' becomes "aCd" or "aDd"
-    var 'D' becomes "bDc" or "bc"
+//    var 'S' becomes "AB"  or "C"
+//    var 'A' becomes "aAb" or "ab"
+//    var 'B' becomes "cBd" or "cd"
+//    var 'C' becomes "aCd" or "aDd"
+//    var 'D' becomes "bDc" or "bc"
+    var 'S' becomes "AB"
+    var 'A' becomes "BB" or "a"
+    var 'B' becomes "AB" or "b"
 }
 
-println g
+//CYKResult table = g.getGrammar().getCYKResult("bbaab")
+CYKResult table = g.getGrammar().getCYKResult("aabba")
+
+
+table.table.each {
+    it.each {
+        print "$it\t"
+    }
+    println()
+}
